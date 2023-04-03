a54b83bf701b2f1babd961963c9838ea22f61f95
wonderf00l <maks22036@gmail.com>
2023-04-03 16:31:11 +0300

    reverted Revert "init"
    This reverts commit 4b1542b4a935c3640e1c27b82a44c3029efd90a7.

--- module1/test/comp.cpp
+++
@@ -1,6 +0,0 @@
-#include "comp.hpp"
-
-template <typename T>
-bool DefaultComparator<T>::operator()(const T& lhs, const T& rhs) const {
-    return lhs < rhs;
-}
--- module1/test/comp.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-template <typename T>
-struct DefaultComparator {
-    bool operator()(const T& lhs, const T& rhs) const;
-};
-
-// шаблонный компаратор нужен, чтобы задавать алгоритм сравнения при инициализации
